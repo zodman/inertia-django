f857fe36d83339d574bb6db0128cf4fa10d8d19b
Andres Vargas <andrevargas@coca-cola.com>
2020-02-19 21:54:49 -0600

    version

--- inertia/version.py
+++ inertia/version.py
@@ -6,16 +6,13 @@ class Version:
 
     def set_version(self, version):
         self.asset_version = version
-        #print(f"VERSION: {self.asset_version}")
 
     def get_version(self):
         if callable(self.asset_version):
-            #print(f"VERSION: {self.asset_version}")
             return self.asset_version()
         else:
-            #print(f"VERSION: {self.asset_version}")
             return self.asset_version
 
 
 asset_version = Version()
-asset_version.set_version(getattr(settings,"VERSIOn",1))
+asset_version.set_version(getattr(settings,"VERSION",1))
