57ddad3f9d8eca271e4b56c9808680154d2cee6d
Andres Vargas <andrevargas@coca-cola.com>
2020-08-17 01:56:14 -0500

    fix indentation

--- inertia/middleware.py
+++ inertia/middleware.py
@@ -1,11 +1,13 @@
+from .share import share
+
 class InertiaMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
 
     def __call__(self, request):
-		share(request, "flash", {'success':request.session.get("success",False),
-                                 'error':request.session.get("error", False)})
+        share(request, "flash", {'success': request.session.get("success",False),
+                                 'error': request.session.get("error", False)})
         share(request, 'errors', request.session.get("errors",[]) )
         response = self.get_response(request)
         return response
