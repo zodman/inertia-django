ba9ab77260480141ab73c856b3c2a654c4f8ba3e
Andres Vargas <andres@python3.ninja>
2020-08-24 08:57:10 -0500

    update setup.cfg

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/zodman/inertia-django'
 EMAIL = 'zodman@gmail.com'
 AUTHOR = 'Andres vargas'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.7'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
